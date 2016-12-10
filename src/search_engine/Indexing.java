dea559e935154b0b97410068642604bd667b4079
ElenaTriant <eltriant@hotmail.gr>
2016-12-10 21:55:54 +0200

    Add files via upload

---
+++ src/search_engine/Indexing.java
@@ -0,0 +1,50 @@
+//package index;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Indexing {
+
+	SampleKeys kwd = new SampleKeys();
+
+	HashMap<Integer, ArrayList<String>> hash = kwd.findKeywords();
+
+	HashMap<String, ArrayList<Integer>> index = new HashMap<String, ArrayList<Integer>>();
+
+
+
+	public HashMap<String, ArrayList<Integer>> getTable() {
+
+		int i = 1;
+		int end = hash.size();
+
+		ArrayList<String> array = new ArrayList<String>();
+
+
+		while(i <= end) {  //end=3
+
+			array = hash.get(i);
+
+			for (int x = 0; x < array.size(); x++) {
+				ArrayList<Integer> ids = new ArrayList<Integer>();
+
+				if (index.containsKey(array.get(x))) {
+					index.get(array.get(x)).add(i);
+				} else {
+					if (ids.contains(i)) {
+						index.put(array.get(x), ids);
+					} else {
+						ids.add(i);
+						index.put(array.get(x), ids);
+					}
+				}
+
+		    }
+
+		    i++;
+		}
+
+		return index;
+	}
+
+}
---
+++ src/search_engine/TestIndexing.java
@@ -0,0 +1,26 @@
+//package index;
+
+import java.lang.NullPointerException;
+
+public class TestIndexing {
+
+	public static void main(String[] args) {
+		SampleKeys test = new SampleKeys();
+		Indexing test2 = new Indexing();
+
+		try {
+			System.out.println(test.getKeywords());
+			System.out.println();
+
+			System.out.println(test2.getTable());
+		} catch (NullPointerException e) {
+			System.out.println("no filled hasmap");
+
+		}
+
+
+
+	}
+
+
+}
