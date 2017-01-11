3e2896a60944688c58369400fbff811b42330e8e
Malvina Papadaki <t8150108@dias.aueb.gr>
2017-01-11 14:58:32 +0200

    Update Indexing.java

--- src/search_engine/Indexing.java
+++ src/search_engine/Indexing.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 
 public class Indexing {
 
-    /** Creates the index that matches keywords with the urls they are found
+    /** Gets a map containing links and keywords and creates an Index that matches each keyword
+     * with the pages it can be found
      * 
      * @return HashMap<String, ArrayList<Integer>>
      * @throws SQLException
@@ -14,10 +15,11 @@ public class Indexing {
      */
 	public HashMap<String, ArrayList<Integer>> getTable() throws SQLException, Exception {
 		
+		//gets the Hashmap that matches links with their keywords
 		Keywords kwd = new Keywords();
 
 		HashMap<Integer, ArrayList<String>> hash = kwd.findKeywords();
-	    HashMap<String, ArrayList<Integer>> index = new HashMap<String, ArrayList<Integer>>();
+	        HashMap<String, ArrayList<Integer>> index = new HashMap<String, ArrayList<Integer>>();
 
 		int i = 1;
 		int end = hash.size();
@@ -30,9 +32,11 @@ public class Indexing {
 			array = hash.get(i);
 
 			for (int x = 0; x < array.size(); x++) {
+				//ignore the specified stop-words
 				if (array.get(x).equals("και")||array.get(x).equals("ο")||array.get(x).equals("η")||array.get(x).equals("το")||array.get(x).equals("για")) {
 					continue;
 				}
+				//create index
 				ArrayList<Integer> ids = new ArrayList<Integer>();
 
 				if (index.containsKey(array.get(x))) {
